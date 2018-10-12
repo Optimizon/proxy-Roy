ab20e3a60cc7d47ee36d6446025895c8968fc489
Roy Lee <roylee@Andrews-MacBook-Pro.local>
2018-10-12 15:56:00 -0700

    included loaderio and everones component

--- server.js
+++ server.js
@@ -10,42 +10,46 @@ const newRelic = require("newrelic");
 // app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// AVH
-// app.get('/api/reviews/:productId', (req, res) => {
-//   fetch(`http://ec2-54-193-70-8.us-west-1.compute.amazonaws.com:80/reviews/${req.params.productId}`)
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .then(json => res.send(json));
-// });
+app.use('/loaderio-6981f2e3cf3c0b995343aa1a6351ca50', (req, res) => {
+        res.sendFile(__dirname + '/loaderio-6981f2e3cf3c0b995343aa1a6351ca50.txt');
+})
 
-// AVH
-// app.get('/api/helpful/:productId', (req, res) => {
-//   fetch(`http://ec2-54-193-70-8.us-west-1.compute.amazonaws.com:80/helpful/${req.params.productId}`)
-//     .then((res) => {
-//       // do nothing
-//     })
-//     .then(json => res.status(202).send());
-// });
+app.get('/api/reviews/:productId', (req, res) => {
+  fetch(`http://ec2-18-144-29-61.us-west-1.compute.amazonaws.com/api/reviews/${req.params.productId}`)
+    .then((res) => {
+      // console.log(res);
+      return res.json();
+    })
+    .then(json => res.send(json));
+});
+
+app.post('/api/reviews/:productId', (req, res) => {
+  fetch(`http://ec2-18-144-29-61.us-west-1.compute.amazonaws.com/api/reviews/new`, {
+    method: 'POST'
+  })
+    .then((res) => {
+      // do nothing
+    })
+    .then(json => res.status(202).send());
+});
 
 // Matt
-// app.get('/api/checkout/:productId', (req, res) => {
-//   fetch(`http://ec2-18-224-5-50.us-east-2.compute.amazonaws.com:80/checkout/${req.params.productId}`)
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .then(json => res.send(json));
-// });
+app.get('/checkout/:productId', (req, res) => {
+  fetch(`http://ec2-18-212-122-128.compute-1.amazonaws.com:7777/checkout/${req.params.productId}`)
+    .then(res => res.json())
+    .then(json => res.send(json));
+});
 
 // Sonia
-// http://ec2-13-57-246-165.us-west-1.compute.amazonaws.com:3036/?id=1
-// app.get('/api/relatedItems', (req, res) => {
-//   fetch(`http://ec2-13-57-246-165.us-west-1.compute.amazonaws.com:3036/product?id=${req.query.id}`)
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .then(json => res.send(JSON.stringify(json)));
-// });
+
+  app.get('/product', (req, res) => { //will be sent to my component 
+    // console.log("here", req.query.id) 
+      fetch(`http://ec2-52-53-250-252.us-west-1.compute.amazonaws.com:4043/product/?id=${req.query.id}`) //will proxy to my component server 
+        .then((res) => {
+          return res.json();
+        })
+        .then(json => res.send(JSON.stringify(json)));
+    })
 
 // Michelle
 // http://ec2-13-57-32-246.us-west-1.compute.amazonaws.com/?id=2
@@ -55,7 +59,8 @@ app.get('/get', (req, res) => {
       return response.json()
     }).then(json => {
       res.send(json)
-    });
+    })
+    .catch(err => console.log(err))
 });
 
 app.listen(port, () => {
